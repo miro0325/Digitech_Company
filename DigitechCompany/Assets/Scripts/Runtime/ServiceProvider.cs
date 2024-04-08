ca292f1419e4f01a54cdd81478fd5c941039c4a3
proffeine0327 <proffeine0327@gmail.com>
2024-04-08 13:12:50 +0900

    #7 ServiceProvider에 Game namespace 추가

--- DigitechCompany/Assets/Scripts/Runtime/ServiceProvider.cs
+++ DigitechCompany/Assets/Scripts/Runtime/ServiceProvider.cs
@@ -4,72 +4,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ServiceProvider : MonoBehaviour
+namespace Game
 {
-    private static ServiceProvider _instance;
-
-    public static bool Register<T>(T instance, bool isGlobal = false) where T : MonoBehaviour
+    public class ServiceProvider : MonoBehaviour
     {
-        if (_instance == null)
+        private static ServiceProvider _instance;
+
+        public static bool Register<T>(T instance, bool isGlobal = false) where T : MonoBehaviour
         {
-            _instance = new GameObject("[ServiceProvider]").AddComponent<ServiceProvider>();
-            DontDestroyOnLoad(_instance.gameObject);
-            SceneManager.sceneUnloaded += _ =>
+            if (_instance == null)
             {
-                foreach(var context in new Dictionary<string, MonoBehaviour>(_instance.sceneContext))
+                _instance = new GameObject("[ServiceProvider]").AddComponent<ServiceProvider>();
+                DontDestroyOnLoad(_instance.gameObject);
+                SceneManager.sceneUnloaded += _ =>
                 {
-                    if(context.Value == null)
-                        _instance.sceneContext.Remove(context.Key);
+                    foreach (var context in new Dictionary<string, MonoBehaviour>(_instance.sceneContext))
+                    {
+                        if (context.Value == null)
+                            _instance.sceneContext.Remove(context.Key);
+                    }
+                };
+            }
+
+            if (isGlobal)
+            {
+                if (!_instance.globalContext.ContainsKey(typeof(T).FullName))
+                {
+                    _instance.globalContext.Add(typeof(T).FullName, instance);
+                    DontDestroyOnLoad(instance.gameObject);
+                    return true;
                 }
-            };
-        }
-
-        if (isGlobal)
-        {
-            if (!_instance.globalContext.ContainsKey(typeof(T).FullName))
-            {
-                _instance.globalContext.Add(typeof(T).FullName, instance);
-                DontDestroyOnLoad(instance.gameObject);
-                return true;
+                else
+                {
+                    Debug.LogWarning($"{typeof(T).FullName} type already exist in global context.");
+                    Destroy(instance.gameObject);
+                    return false;
+                }
             }
             else
             {
-                Debug.LogWarning($"{typeof(T).FullName} type already exist in global context.");
-                Destroy(instance.gameObject);
-                return false;
+                if (!_instance.sceneContext.ContainsKey(typeof(T).FullName))
+                {
+                    _instance.sceneContext.Add(typeof(T).FullName, instance);
+                    return true;
+                }
+                else
+                {
+                    Debug.LogWarning($"{typeof(T).FullName} type already exist in scene context.");
+                    Destroy(instance.gameObject);
+                    return false;
+                }
             }
         }
-        else
-        {
-            if (!_instance.sceneContext.ContainsKey(typeof(T).FullName))
-            {
-                _instance.sceneContext.Add(typeof(T).FullName, instance);
-                return true;
-            }
-            else
-            {
-                Debug.LogWarning($"{typeof(T).FullName} type already exist in scene context.");
-                Destroy(instance.gameObject);
-                return false;
-            }
-        }
-    }
 
-    public static T Get<T>() where T : MonoBehaviour
-    {
-        if (_instance.sceneContext.ContainsKey(typeof(T).FullName))
+        public static T Get<T>() where T : MonoBehaviour
         {
-            T inst = _instance.sceneContext[typeof(T).FullName] as T;
-            return inst;
+            if (_instance.sceneContext.ContainsKey(typeof(T).FullName))
+            {
+                T inst = _instance.sceneContext[typeof(T).FullName] as T;
+                return inst;
+            }
+            if (_instance.globalContext.ContainsKey(typeof(T).FullName))
+            {
+                T inst = _instance.globalContext[typeof(T).FullName] as T;
+                return inst;
+            }
+            return null;
         }
-        if(_instance.globalContext.ContainsKey(typeof(T).FullName))
-        {
-            T inst = _instance.globalContext[typeof(T).FullName] as T;
-            return inst;
-        }
-        return null;
-    }
 
-    private readonly Dictionary<string, MonoBehaviour> globalContext = new();
-    private readonly Dictionary<string, MonoBehaviour> sceneContext = new();
+        private readonly Dictionary<string, MonoBehaviour> globalContext = new();
+        private readonly Dictionary<string, MonoBehaviour> sceneContext = new();
+    }
 }
