7fd8c42cc3c2811a26808c44ed373e7d3027fe07
proffeine0327 <proffeine0327@gmail.com>
2024-04-09 10:04:43 +0900

    #2 Unitask 추가, 구글 스프레드시트에서 데이터 불러오기

---
+++ DigitechCompany/Assets/Scripts/Runtime/Data/ItemData.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Game.Data
+{
+    public class ItemData
+    {
+        public static ItemData Parse(string tsvRow)
+        {
+            return new();
+        }
+    }
+}
