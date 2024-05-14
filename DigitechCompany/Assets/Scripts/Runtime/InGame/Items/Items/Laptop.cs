14306abf42e889a2352f04357aaa1ee069e70d6f
PotatoCat0325 <hanc02095@gmail.com>
2024-05-14 21:47:40 +0900

    Change Laptop Animation

--- DigitechCompany/Assets/Scripts/Runtime/InGame/Items/Items/Laptop.cs
+++ DigitechCompany/Assets/Scripts/Runtime/InGame/Items/Items/Laptop.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Photon.Pun;
 
 public class Laptop : ItemBase
 {
-    private readonly int controlAnim = Animator.StringToHash("ControlLaptop");
+    private readonly int closeAnim = Animator.StringToHash("Close");
+    private readonly int openAnim = Animator.StringToHash("Open");
     private readonly int textureID = Shader.PropertyToID("_MainTex");
 
     [SerializeField]
     private MeshRenderer[] renderers;
+
+    
 
     [SerializeField]
     private Texture2D[] textures;
 
     private bool isOpen = false;
     private bool isPlaying = false;
-    
+
     protected override void Update()
     {
         
+    }
+
+    public override void OnInteract(UnitBase unit)
+    {
+        base.OnInteract(unit);
+        animator.Play("Idle");
+    }
+
+    public override bool IsUsable(InteractID id)
+    {
+        if(id == InteractID.ID2) return true;
+        return false;
     }
 
     public override void OnUse(InteractID id)
@@ -27,7 +44,16 @@ public class Laptop : ItemBase
         base.OnUse(id);
         isOpen = !isOpen;
         isPlaying = true;
-        animator.SetBool(controlAnim, isOpen);
+        var anim = (isOpen ? openAnim : closeAnim);
+        animator.Play(anim);
+    }
+
+    protected override void OnUseRpc(int id)
+    {
+        isOpen = !isOpen;
+        isPlaying = true;
+        var anim = (isOpen ? openAnim : closeAnim);
+        animator.Play(anim);
     }
 
     public void ControlLight()
@@ -37,9 +63,27 @@ public class Laptop : ItemBase
         {
             renderer.material.SetTexture(textureID, tex);
         }
+        photonView.RPC(nameof(ControlLightRPC), RpcTarget.Others);
+    }
+
+    [PunRPC]
+    private void ControlLightRPC()
+    {
+        var tex = textures[(isOpen) ? 0 : 1];
+        foreach (var renderer in renderers)
+        {
+            renderer.material.SetTexture(textureID, tex);
+        }
     }
 
     public void EndAnimation()
+    {
+        isPlaying = false;
+        photonView.RPC(nameof(EndAnimationRPC), RpcTarget.Others);
+    }
+
+    [PunRPC]
+    private void EndAnimationRPC()
     {
         isPlaying = false;
     }
